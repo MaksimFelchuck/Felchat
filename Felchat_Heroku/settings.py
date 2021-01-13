0a78d16902b44be4f13b5062595b6f0414b710f4
Макс <felchuck@yandex.ru>
2021-01-14 02:54:27 +0700

    postgress

--- Felchat_Heroku/settings.py
+++ Felchat_Heroku/settings.py
@@ -84,7 +84,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
